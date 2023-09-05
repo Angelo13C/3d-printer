646e5e613f9b9c9e79e570fea35dca6bdabe72df
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-09-05 22:05:04 +0200

    ✨ Add constrain function in the math module

--- firmware/crates/core/src/utils/math/mod.rs
+++ firmware/crates/core/src/utils/math/mod.rs
@@ -10,3 +10,31 @@ where T: Clone + Copy + Sub<T, Output = T> + Mul<T, Output = T> + Add<T, Output 
 {
 	(value - *from.start()) * (*to.end() - *to.start()) / (*from.end() - *from.start()) + *to.start()
 }
+
+/// Returns `value` if `value` is contained in the provided `range`, `range.start()` if `value` is smaller
+/// than `range.start()` and `range.end()` if `value` is greater than `range.end()`.
+///
+/// # Examples
+/// ```
+/// # use firmware_core::utils::math::constrain;
+/// #
+/// assert_eq!(constrain(10, 0..=20), 10);
+/// assert_eq!(constrain(-5, 0..=20), 0);
+/// assert_eq!(constrain(40, 0..=20), 20);
+/// ```
+pub fn constrain<T>(value: T, range: RangeInclusive<T>) -> T
+where T: Copy + PartialOrd
+{
+	if value < *range.start()
+	{
+		*range.start()
+	}
+	else if value > *range.end()
+	{
+		*range.end()
+	}
+	else
+	{
+		value
+	}
+}
