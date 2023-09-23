92109e9c983632ffa62c2141cd8a678b034242dd
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-09-23 11:45:31 +0200

    ✨ Add Kinematics trait

---
+++ firmware/crates/core/src/printer/components/motion/kinematics/mod.rs
@@ -0,0 +1,16 @@
+use crate::utils::measurement::distance::Distance;
+
+/// Type that represents the kinematics of the machine (i.e. how the motion of the `a` and `b` motors effects
+/// the movement of the tool carriage).
+pub trait Kinematics
+{
+	/// Converts a displacement on the XY coordinate system to the displacement required to be done by the `a` motor.
+	fn xy_displacement_to_a(x: Distance, y: Distance) -> Distance;
+	/// Converts a displacement on the XY coordinate system to the displacement required to be done by the `b` motor.
+	fn xy_displacement_to_b(x: Distance, y: Distance) -> Distance;
+
+	/// Converts a displacement done by the `a` and `b` motors to a displacement of the tool on the X axis.
+	fn ab_displacement_to_x(a: Distance, b: Distance) -> Distance;
+	/// Converts a displacement done by the `a` and `b` motors to a displacement of the tool on the Y axis.
+	fn ab_displacement_to_y(a: Distance, b: Distance) -> Distance;
+}
