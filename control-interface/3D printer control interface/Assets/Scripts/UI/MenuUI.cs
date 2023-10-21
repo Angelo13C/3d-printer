5177658c6bb98766bd154de01b90332b7c619a82
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-10-21 21:45:03 +0200

    ✨ Add MenuUI functionality in control-interface

---
+++ control-interface/3D printer control interface/Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MenuUI : MonoBehaviour
+    {
+        [SerializeField] private Canvas[] _menus;
+
+        private Canvas _currentlySelectedMenu = null;
+        
+        private void Awake()
+        {
+            foreach (var menu in _menus)
+                menu.enabled = false;
+        }
+
+        public void OpenMenu(Canvas menu)
+        {
+            if (_currentlySelectedMenu != null)
+                _currentlySelectedMenu.enabled = false;
+
+            _currentlySelectedMenu = menu;
+            _currentlySelectedMenu.enabled = true;
+        }
+    }
+}
