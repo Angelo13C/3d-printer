b0c88d94c70bfbd8e2beee8e2578f635a933398f
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-10-21 16:42:40 +0200

    ✨ Add ConnectionUI functionality scripts in control-interface

---
+++ control-interface/3D printer control interface/Assets/Scripts/Client/Connectivity/Connection.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+using UnityEngine;
+
+namespace Client.Connectivity
+{
+    public abstract class Connection : MonoBehaviour
+    {
+        public abstract bool IsConnected();
+        protected abstract bool SendIfPossible(HttpRequestMessage request);
+    }
+}
