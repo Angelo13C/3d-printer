bb6578a64603b1d1e1eeccb8baca52443bb068f5
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-10-22 16:06:54 +0200

    ✨ Add SendFileToPrinter functionality in control-interface

---
+++ control-interface/3D printer control interface/Assets/Scripts/UI/Menus/Files/SendFileToPrinter.cs
@@ -0,0 +1,45 @@
+using System.IO;
+using System.Net.Http;
+using Client;
+using SFB;
+using UnityEngine;
+
+namespace UI.Menus.Files
+{
+    public class SendFileToPrinter : MonoBehaviour
+    {
+        [SerializeField] private HttpsClient _httpsClient;
+    
+        public void OpenFilePicker()
+        {
+            if (_httpsClient == null)
+                return;
+        
+            var extensions = new [] {
+                new ExtensionFilter("G-code File ", "gcode"),
+            };
+            StandaloneFileBrowser.OpenFilePanelAsync("Open File", "", extensions, false, async paths =>
+            {
+                foreach (var path in paths)
+                {
+                    if (File.Exists(path))
+                    {
+                        var file = File.OpenRead(path);
+                        var content = new StreamContent(file);
+                        var request = new HttpRequestMessage
+                        {
+                            Method = HttpMethod.Post,
+                            Content = content,
+                            Headers =
+                            {
+                                { "length", file.Length.ToString() },
+                                { "name", file.Name }
+                            }
+                        };
+                        await _httpsClient.SendRequest(request, RequestType.SendFile);
+                    }
+                }
+            });
+        }
+    }
+}
