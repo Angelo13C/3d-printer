584ca2adfeb41b1e79d201d1663f5532608791d8
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-10-23 15:45:36 +0200

    🚧 Add SettingsFieldsDisplay basic functionality in control-interface

---
+++ control-interface/3D printer control interface/Assets/Scripts/UI/Menus/Settings/SettingsFieldsDisplay.cs
@@ -0,0 +1,68 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace UI.Menus.Settings
+{
+    public class SettingsFieldsDisplay : MonoBehaviour
+    {
+        [SerializeField] private SettingsFields _settingsFields;
+        
+        [Header("Prefabs")]
+        [SerializeField] private GameObject _blockPrefab;
+        [SerializeField] private GameObject _fieldPrefab;
+
+        [Header("Customization")] 
+        [SerializeField] private float _rightPaddingForUnits = 20;
+        
+        private void Awake()
+        {
+            foreach (var block in _settingsFields.Blocks)
+            {
+                var blockGameObject = Instantiate(_blockPrefab, transform);
+                blockGameObject.GetComponent<TextMeshProUGUI>().text = block.Title;
+
+                foreach (var field in block.Fields)
+                {
+                    var fieldGameObject = Instantiate(_fieldPrefab, blockGameObject.transform);
+                    fieldGameObject.GetComponentInChildren<TextMeshProUGUI>().text = field.Name;
+
+                    var inputField = fieldGameObject.GetComponentInChildren<TMP_InputField>();
+                    
+                    ApplyFieldTypeSettings(field.Type, inputField);
+                }
+            }
+        }
+
+        private void ApplyFieldTypeSettings(SettingsFields.Block.Field.FieldType fieldType, TMP_InputField inputField)
+        {
+            switch (fieldType)
+            {
+                case SettingsFields.Block.Field.FieldType.Int:
+                    inputField.contentType = TMP_InputField.ContentType.IntegerNumber;
+                    break;
+                case SettingsFields.Block.Field.FieldType.Distance:
+                    inputField.contentType = TMP_InputField.ContentType.DecimalNumber;
+
+                    var indexOfUnits = 0;
+                    var width = 0f;
+                    for (; indexOfUnits < inputField.transform.childCount; indexOfUnits++)
+                    {
+                        var unitsText = inputField.transform.GetChild(indexOfUnits).GetComponent<TextMeshProUGUI>();
+                        if (unitsText != null)
+                        {
+                            unitsText.text = "mm";
+                            unitsText.ForceMeshUpdate();
+                            width = unitsText.textBounds.size.x;
+                            break;
+                        }
+                    }
+                    
+                    var indexOfTextArea = indexOfUnits == 0 ? 1 : 0;
+                    var textAreaTransform = inputField.transform.GetChild(indexOfTextArea).GetComponent<RectTransform>();
+                    textAreaTransform.offsetMax = new Vector2(-(width + _rightPaddingForUnits), textAreaTransform.offsetMax.y);
+                    
+                    break;
+            }
+        }
+    }
+}
