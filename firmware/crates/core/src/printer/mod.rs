77cc4148f07ba6e59fa35e068b9b8b027af1317a
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-11-02 16:36:44 +0100

    🐛 Fix compilation error

--- firmware/crates/core/src/printer/mod.rs
+++ firmware/crates/core/src/printer/mod.rs
@@ -12,7 +12,8 @@ impl<P: Peripherals> Printer3D<P>
 	pub fn new(mut peripherals: P, components_config: ComponentsConfig) -> Result<Self, CreationError>
 	{
 		Ok(Self {
-			components: Printer3DComponents::new(&mut peripherals, components_config)?,
+			components: Printer3DComponents::new(&mut peripherals, components_config)
+				.map_err(CreationError::Components)?,
 		})
 	}
 
