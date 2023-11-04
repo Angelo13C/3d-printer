e85c14637aae5b28188c85fcfc68eee841dd7925
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-11-04 09:24:23 +0100

    🤡 Mock UART

--- firmware/crates/core/src/printer/components/mock/mod.rs
+++ firmware/crates/core/src/printer/components/mock/mod.rs
@@ -6,6 +6,7 @@ mod pwm;
 mod spi;
 mod time;
 mod timer;
+mod uart;
 mod z_axis_probe;
 
 pub use adc::*;
@@ -16,6 +17,7 @@ pub use pwm::*;
 pub use spi::*;
 pub use time::*;
 pub use timer::*;
+pub use uart::*;
 pub use z_axis_probe::*;
 
 #[derive(Debug)]
