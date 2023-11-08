fe8edb6def7fb941b9719522754a439c273ba2e5
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-11-08 23:09:26 +0100

    ✨ Add SystemTime, PrintProcess and GCodeHistory to the resources of http uri handlers

--- firmware/crates/core/src/printer/communication/http/resources.rs
+++ firmware/crates/core/src/printer/communication/http/resources.rs
@@ -1,10 +1,10 @@
 use std::sync::Arc;
 
-use super::command::CommandsSender;
+use super::{command::CommandsSender, other::GCodeHistory};
 use crate::{
 	printer::{
 		communication::security::Security,
-		components::{file_system::FileSystem, Peripherals},
+		components::{file_system::FileSystem, print_process::PrintProcess, Peripherals},
 	},
 	utils::mutex::{Mutex, MutexGuard},
 };
@@ -13,21 +13,29 @@ pub struct Resources<P: Peripherals>(Arc<Mutex<ResourcesImpl<P>>>);
 
 pub struct ResourcesImpl<P: Peripherals>
 {
+	pub system_time: Option<P::SystemTime>,
 	pub file_system: FileSystem<P::FlashChip, P::FlashSpi>,
 	pub security: Security,
 	pub command_sender: CommandsSender<P>,
+	pub print_process: PrintProcess<P>,
+
+	pub g_code_history: GCodeHistory,
 }
 
 impl<P: Peripherals> Resources<P>
 {
 	pub fn new(
-		file_system: FileSystem<P::FlashChip, P::FlashSpi>, security: Security, command_sender: CommandsSender<P>,
+		system_time: Option<P::SystemTime>, file_system: FileSystem<P::FlashChip, P::FlashSpi>, security: Security,
+		command_sender: CommandsSender<P>, print_process: PrintProcess<P>,
 	) -> Self
 	{
 		Self(Arc::new(Mutex::new(ResourcesImpl {
+			system_time,
 			file_system,
 			security,
 			command_sender,
+			print_process,
+			g_code_history: GCodeHistory::new(),
 		})))
 	}
 
