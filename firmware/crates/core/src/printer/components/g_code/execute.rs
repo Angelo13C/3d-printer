2dd9816376c2298f653d67293a3fc1bce8f5e341
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-11-08 22:55:32 +0100

    📝 Add documentation to some parts of GCodeExecuter

--- firmware/crates/core/src/printer/components/g_code/execute.rs
+++ firmware/crates/core/src/printer/components/g_code/execute.rs
@@ -9,12 +9,17 @@ use crate::{
 	},
 };
 
+/// Number of savable positions in `GCodeExecuter` (check [`GCodeExecuter::save_position`] and [`GCodeExecuter::get_saved_position`]).
 pub const SAVED_POSITIONS_COUNT: usize = 1;
 
 pub struct GCodeExecuter<P: Peripherals>
 {
+	/// All the commands added to the executer first go in this queue to be prepared.
 	commands_to_prepare: VecDeque<Box<dyn GCodeCommand<P>>>,
+	/// When a command is successfully prepared (it returns [`Status::Finished`]), it goes in this queue.
 	command_buffer: VecDeque<Box<dyn GCodeCommand<P>>>,
+	/// This is the first command taken from the `command_buffer` queue and it's constantly executed. When the execution is finished
+	/// (it returns [`Status::Finished`]) this field becomes `None` and a new command is taken (if possible) from the `command_buffer`.
 	current_command: Option<Box<dyn GCodeCommand<P>>>,
 
 	position_mode: PositionMode,
@@ -33,7 +38,7 @@ impl<P: Peripherals> GCodeExecuter<P>
 			{
 				Status::Working => self.commands_to_prepare.push_front(command),
 				Status::Finished => self.command_buffer.push_back(command),
-				Status::Error(error) => return Err(TickError::PreparingCommand(error)),
+				Status::Error(error) => return Err(TickError::PreparingCommand { error }),
 			}
 		}
 
@@ -50,7 +55,7 @@ impl<P: Peripherals> GCodeExecuter<P>
 			{
 				Status::Working => self.current_command = Some(command),
 				Status::Finished => (),
-				Status::Error(error) => return Err(TickError::ExecutingCommand(error)),
+				Status::Error(error) => return Err(TickError::ExecutingCommand { error }),
 			}
 		}
 
@@ -110,6 +115,19 @@ impl<P: Peripherals> GCodeExecuter<P>
 		self.commands_to_prepare.push_back(command);
 	}
 
+	/// Save the provided `position` at the specified `slot`, so that you can later retrieve it using [`Self::get_position(slot)`].
+	///
+	/// Returns `Err(InvalidPositionSlot)` if `slot >= SAVED_POSITIONS_COUNT`.
+	///
+	/// # Examples
+	/// ```
+	/// # use firmware_core::{printer::components::{g_code::execute::*, mock::*}, utils::math::vectors::*};
+	/// #
+	/// let mut g_code_executer = GCodeExecuter::<MockPeripherals>::default();
+	///
+	/// assert!(g_code_executer.save_position(VectorN::default(), 0).is_ok());
+	/// assert!(g_code_executer.save_position(VectorN::default(), 1000).is_err());
+	/// ```
 	pub fn save_position(&mut self, position: VectorN<4>, slot: usize) -> Result<(), InvalidPositionSlot>
 	{
 		self.validate_position_slot(slot)?;
@@ -119,6 +137,24 @@ impl<P: Peripherals> GCodeExecuter<P>
 		Ok(())
 	}
 
+	/// Get the position you provided to [`Self::save_position`] at the same slot as the one you provided here (or [`VectorN::default`]
+	/// if you never saved a position in that slot).
+	///
+	/// Returns `Err(InvalidPositionSlot)` if `slot >= SAVED_POSITIONS_COUNT`.
+	///
+	/// # Examples
+	/// ```
+	/// # use firmware_core::{printer::components::{g_code::execute::*, mock::*}, utils::{measurement::distance::Distance, math::vectors::*}};
+	/// #
+	/// let mut g_code_executer = GCodeExecuter::<MockPeripherals>::default();
+	///
+	/// assert_eq!(g_code_executer.get_saved_position(0), Ok(VectorN::default()));
+	///
+	/// g_code_executer.save_position(VectorN::new([Distance::from_centimeters(3); 4]), 0).unwrap();
+	/// assert_eq!(g_code_executer.get_saved_position(0), Ok(VectorN::new([Distance::from_centimeters(3); 4])));
+	///
+	/// assert_eq!(g_code_executer.get_saved_position(100000), Err(InvalidPositionSlot));
+	/// ```
 	pub fn get_saved_position(&self, slot: usize) -> Result<VectorN<4>, InvalidPositionSlot>
 	{
 		self.validate_position_slot(slot)?;
@@ -139,8 +175,16 @@ impl<P: Peripherals> GCodeExecuter<P>
 #[derive(Debug)]
 pub enum TickError
 {
-	PreparingCommand(String),
-	ExecutingCommand(String),
+	/// Calling [`GCodeCommand::prepare`] resulted in the [`Status::Error`] being returned.
+	PreparingCommand
+	{
+		error: String
+	},
+	/// Calling [`GCodeCommand::execute`] resulted in the [`Status::Error`] being returned.
+	ExecutingCommand
+	{
+		error: String
+	},
 }
 
 impl<P: Peripherals> Default for GCodeExecuter<P>
@@ -158,6 +202,7 @@ impl<P: Peripherals> Default for GCodeExecuter<P>
 	}
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct InvalidPositionSlot;
 
 #[derive(Default, Clone, Copy, PartialEq, Eq, Debug)]
