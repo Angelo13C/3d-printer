8ebe2aa2941fe3c9089d7e83e474e54fc0a5d0b7
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-11-14 12:18:15 +0100

    ✨ Add the Communicator trait

---
+++ firmware/crates/core/src/printer/communication/communicator/mod.rs
@@ -0,0 +1,36 @@
+use std::fmt::Debug;
+
+use embedded_hal::spi::SpiDevice;
+use embedded_svc::http::{
+	server::{Connection, HandlerResult, Request},
+	Method,
+};
+use strum::IntoEnumIterator;
+
+use super::http::{request::HttpRequest, resources::Resources};
+use crate::printer::components::{drivers::spi_flash_memory::FlashMemoryChip, Peripherals};
+
+/// A type that implements this trait can be used to make the microcontroller communicate with the external
+/// world.
+pub trait Communicator
+{
+	type Error: Debug;
+
+	/// Make the [Communicator] register the handle all the possible [`requests`] supported by this firmware.
+	///
+	/// [`requests`]: HttpRequest
+	fn register_all_requests<P: Peripherals + 'static>(&mut self, resources: Resources<P>) -> Result<(), Self::Error>
+	{
+		for possible_request in HttpRequest::iter()
+		{
+			self.register_request(possible_request, resources.clone())?;
+		}
+
+		Ok(())
+	}
+
+	/// Make the [Communicator] register the handle of the provided `request`.
+	fn register_request<P: Peripherals + 'static>(
+		&mut self, request: HttpRequest, resources: Resources<P>,
+	) -> Result<(), Self::Error>;
+}
