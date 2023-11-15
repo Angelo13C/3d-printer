b1bfd957a4c3e656d6adb421fa65d4da98eda23d
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-11-15 22:56:08 +0100

    ✨ Set the configuration of the Communication thread

--- firmware/crates/esp32-s3/src/main.rs
+++ firmware/crates/esp32-s3/src/main.rs
@@ -1,7 +1,7 @@
 pub mod config;
 pub mod peripherals;
 
-use esp_idf_hal::peripherals::Peripherals as EspPeripherals;
+use esp_idf_hal::{cpu::Core, peripherals::Peripherals as EspPeripherals};
 use esp_idf_sys::{self as _, EspError}; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use firmware_core::printer::Printer3D;
 use peripherals::Peripherals;
@@ -24,6 +24,16 @@ fn main()
 
 fn create_printer() -> Result<Printer3D<Peripherals>, CreatePrinterError>
 {
+	esp_idf_hal::task::thread::ThreadSpawnConfiguration {
+		name: Some(b"Communication thread"),
+		stack_size: 10_000,
+		priority: 10,
+		inherit: false,
+		pin_to_core: Some(Core::Core1),
+	}
+	.set()
+	.map_err(CreatePrinterError::ThreadSpawnConfiguration)?;
+
 	let peripherals =
 		Peripherals::from_esp_peripherals(EspPeripherals::take().map_err(CreatePrinterError::CantTakeEspPeripherals)?)
 			.map_err(CreatePrinterError::CantCreatePeripherals)?;
@@ -37,6 +47,7 @@ fn create_printer() -> Result<Printer3D<Peripherals>, CreatePrinterError>
 #[derive(Debug)]
 enum CreatePrinterError
 {
+	ThreadSpawnConfiguration(EspError),
 	CantTakeEspPeripherals(EspError),
 	CantCreatePeripherals(EspError),
 	Printer3DCreation(firmware_core::printer::CreationError<Peripherals>),
