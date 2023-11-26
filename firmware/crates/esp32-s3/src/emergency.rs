33927e164cb0ed338c6a6e9bc99d8be4084f8b04
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-11-26 19:23:53 +0100

    🐛 Fix errors being printed by the esp32-s3 when panicking

--- firmware/crates/esp32-s3/src/emergency.rs
+++ firmware/crates/esp32-s3/src/emergency.rs
@@ -1,10 +1,8 @@
-use std::ops::RangeInclusive;
-
 pub unsafe fn disable_all_pins_function()
 {
-	const GPIO_PINS: RangeInclusive<i32> = 0..=100;
+	let gpio_pins = (0..=21).chain(26..=48);
 
-	for gpio_pin in GPIO_PINS
+	for gpio_pin in gpio_pins
 	{
 		esp_idf_sys::gpio_set_level(gpio_pin, 0);
 	}
