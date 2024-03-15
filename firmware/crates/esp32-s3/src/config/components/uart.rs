622dee0d801337e070399de51b08171cedacfcec
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-03-15 18:12:54 +0100

    🐛 Fix the wrong UART configuration to talk to TMC2209 drivers

--- firmware/crates/esp32-s3/src/config/components/uart.rs
+++ firmware/crates/esp32-s3/src/config/components/uart.rs
@@ -4,10 +4,10 @@ use esp_idf_hal::{
 };
 
 pub const UART_CONFIGURATION: Config = Config {
-	baudrate: Hertz(500_000),
+	baudrate: Hertz(115_200),
 	data_bits: DataBits::DataBits8,
 	parity: Parity::ParityNone,
-	stop_bits: StopBits::STOP1P5,
+	stop_bits: StopBits::STOP1,
 	flow_control: FlowControl::None,
 	flow_control_rts_threshold: 0,
 	..Config::new()
