12bc8545e417e273e8b3466f6457d2b977b35ce6
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-07-14 19:02:34 +0200

    🚧 Add most of the bed leveling functionality (it's not finished yet)

--- firmware/crates/core/src/printer/components/motion/bed_leveling/mod.rs
+++ firmware/crates/core/src/printer/components/motion/bed_leveling/mod.rs
@@ -1,3 +1,134 @@
+mod unified;
 mod z_axis_probe;
 
+use enumset::enum_set;
+pub use unified::*;
 pub use z_axis_probe::*;
+
+use super::{
+	axes::Axis,
+	planner::{communicate_to_ticker, BlocksBufferIsFull, Flag, Planner},
+};
+use crate::utils::{
+	math::vectors::{Vector2, VectorN},
+	measurement::distance::Distance,
+};
+
+pub struct BedLevelingProcedure
+{
+	unified_bed_leveling: UnifiedBedLevelingProcedure,
+	bed_size: Vector2,
+	/// The whole purpose of this variable is to solve the problem of the block buffer being full in the middle of the
+	/// bed leveling procedure. With this variable it can re-start from the point where it left off.
+	current_point_planned_index: u16,
+	current_point_index: u16,
+}
+
+impl BedLevelingProcedure
+{
+	/// Movement speed at which the printer is probed.
+	const MOVE_SPEED_MM_SECOND: f32 = 40.;
+	/// Movement speed of probing at which the printer is probed.
+	const PROBE_MOVE_SPEED_MM_SECOND: f32 = 10.;
+	/// Distance of the z axis probe from the bed (z axis) for each probed point.
+	pub const DISTANCE_FROM_BED: Distance = Distance::from_centimeters(3);
+	/// Distance of the z axis probe from the bed margins (x and y axes).
+	const DISTANCE_FROM_BED_MARGINS: Distance = Distance::from_centimeters(3);
+	/// Amount of grid points for the bed leveling procedure (on the x and the y axes).
+	const BED_LEVELING_GRID_SIZE: (u8, u8) = (5, 5);
+
+	pub fn new(bed_size: Vector2) -> Self
+	{
+		Self {
+			unified_bed_leveling: UnifiedBedLevelingProcedure::new(),
+			bed_size,
+			current_point_planned_index: 0,
+			current_point_index: 0,
+		}
+	}
+
+	pub fn start<const N: usize, K: Kinematics>(
+		&mut self, planner: &mut Planner<N>, steps_per_mm: [f32; N],
+	) -> Result<(), BlocksBufferIsFull>
+	{
+		let mut start_y = 0;
+		let mut start_x = 0;
+
+		// Restart from where the procedure stopped due to the block buffer being full
+		if self.current_point_planned_index != 0
+		{
+			start_y = (self.current_point_planned_index / 2) / Self::BED_LEVELING_GRID_SIZE.0 as u16;
+			start_x = (self.current_point_planned_index / 2) % Self::BED_LEVELING_GRID_SIZE.0 as u16;
+		}
+		// It's a new start
+		else
+		{
+			self.current_point_index = 0;
+
+			self.unified_bed_leveling
+				.start(self.bed_size, Self::BED_LEVELING_GRID_SIZE, Distance::ZERO);
+		}
+
+		let distance_between_points = (self.bed_size - &Vector2::new([Self::DISTANCE_FROM_BED_MARGINS * 2; 2]));
+		let distance_between_points = Vector2::from_xy(
+			distance_between_points.x() / Self::BED_LEVELING_GRID_SIZE.0,
+			distance_between_points.y() / Self::BED_LEVELING_GRID_SIZE.1,
+		);
+
+		const START_POSITION: Vector2 = Vector2::new([Self::DISTANCE_FROM_BED_MARGINS; 2]);
+		for y in start_y..Self::BED_LEVELING_GRID_SIZE.1
+		{
+			for x in start_x..Self::BED_LEVELING_GRID_SIZE.0
+			{
+				let mut target_position_array = planner.get_position().clone();
+
+				target_position_array[Axis::X as usize] = START_POSITION.x() + x * distance_between_points.x();
+				target_position_array[Axis::Y as usize] = START_POSITION.y() + y * distance_between_points.y();
+
+				if self.current_point_planned_index % 2 == 0
+				{
+					target_position_array[Axis::Z as usize] = Self::DISTANCE_FROM_BED;
+					let target_position = VectorN::new(target_position_array);
+					planner.plan_move(target_position, steps_per_mm, Self::MOVE_SPEED_MM_SECOND)?;
+					planner.mark_last_added_move_as_ready_to_go();
+					self.current_point_planned_index += 1;
+				}
+
+				target_position_array[Axis::Z as usize] = Distance::from_centimeters(-50);
+				let target_position = VectorN::new(target_position_array);
+				planner.plan_move(target_position, steps_per_mm, Self::PROBE_MOVE_SPEED_MM_SECOND)?;
+				planner.set_flags_on_last_added_block(enum_set!(Flag::BedLeveling));
+				planner.mark_last_added_move_as_ready_to_go();
+				self.current_point_planned_index += 1;
+			}
+
+			start_x = 0;
+		}
+
+		self.current_point_planned_index = 0;
+
+		Ok(())
+	}
+
+	pub fn tick(&mut self)
+	{
+		if let Some(probing_distance) = communicate_to_ticker::get_z_axis_distance()
+		{
+			let point_correction = probing_distance - Self::DISTANCE_FROM_BED;
+			self.unified_bed_leveling
+				.set_point_correction(self.current_point_index, point_correction);
+
+			self.current_point_index += 1;
+
+			if self.current_point_index == Self::BED_LEVELING_GRID_SIZE.0 * Self::BED_LEVELING_GRID_SIZE.1
+			{
+				self.unified_bed_leveling.finish_procedure().unwrap();
+			}
+		}
+	}
+
+	pub fn apply<const N: usize>(&mut self, target_position: &mut VectorN<N>)
+	{
+		self.unified_bed_leveling.apply(target_position)
+	}
+}
---
+++ firmware/crates/core/src/printer/components/motion/bed_leveling/unified.rs
@@ -0,0 +1,171 @@
+use crate::{
+	printer::components::motion::{axes::Axis, kinematics::Kinematics},
+	utils::{
+		math::{
+			self,
+			vectors::{Vector2, VectorN},
+		},
+		measurement::distance::Distance,
+	},
+};
+
+#[derive(Default)]
+pub enum UnifiedBedLevelingProcedure
+{
+	#[default]
+	None,
+	Working(UnifiedBedLeveling),
+	Done(UnifiedBedLeveling),
+}
+
+impl UnifiedBedLevelingProcedure
+{
+	pub fn new() -> Self
+	{
+		Self::default()
+	}
+
+	pub fn has_started(&self)
+	{
+		match self
+		{
+			UnifiedBedLevelingProcedure::Working(_) => true,
+			_ => false,
+		}
+	}
+
+	pub fn start(&mut self, bed_size: Vector2, grid_size: (u8, u8), default_value: Distance)
+	{
+		*self = Self::Working(UnifiedBedLeveling::new(bed_size, grid_size, default_value));
+	}
+
+	pub fn set_point_correction(&mut self, index: u16, point_correction: Distance) -> Result<(), ()>
+	{
+		let function = |inner: &mut UnifiedBedLeveling| {
+			inner.points_correction[index as usize] = point_correction;
+		};
+		match self
+		{
+			UnifiedBedLevelingProcedure::None => return Err(()),
+			UnifiedBedLevelingProcedure::Working(inner) => (function)(inner),
+			UnifiedBedLevelingProcedure::Done(inner) => (function)(inner),
+		}
+
+		Ok(())
+	}
+
+	pub fn finish_procedure(&mut self) -> Result<(), ()>
+	{
+		match self
+		{
+			UnifiedBedLevelingProcedure::Working(inner) =>
+			{
+				*self = Self::Done(std::mem::replace(inner, UnifiedBedLeveling::empty()))
+			},
+			_ => return Err(()),
+		}
+
+		Ok(())
+	}
+
+	pub fn apply<const N: usize>(&mut self, target_position: &mut VectorN<N>)
+	{
+		assert!(N >= 3);
+
+		if let UnifiedBedLevelingProcedure::Done(inner) = self
+		{
+			let x = target_position[Axis::X as usize];
+			let y = target_position[Axis::Y as usize];
+
+			let point_correction = inner.get_point_correction(Vector2::from_xy(x, y));
+			*target_position[Axis::Z as usize] += point_correction;
+		}
+	}
+}
+
+/// # Grid
+/// Points are stored starting from the bottom left, going first on the right and then when a rows ends going up.
+/// Here there is an example for a grid of size 4x5:
+/// ```text
+///              19
+///              ↓
+///        · · · ·
+///        · · · ·
+///        · · · ·
+///    4 → · · · ·
+///    0 → · · · ·
+///        ↑     ↑
+///        0     3
+/// ```
+pub struct UnifiedBedLeveling
+{
+	points_correction: Vec<Distance>,
+	number_of_points_in_x: u8,
+	bed_size: Vector2,
+}
+
+impl UnifiedBedLeveling
+{
+	/// # Panics
+	/// Panics if `grid_size.0` or `grid_size.1` are less than 2.
+	fn new(bed_size: Vector2, grid_size: (u8, u8), default_point_correction: Distance) -> Self
+	{
+		assert!(grid_size.0 > 1 && grid_size.1 > 1);
+
+		Self {
+			points_correction: vec![default_point_correction; grid_size.0 as usize * grid_size.1 as usize],
+			number_of_points_in_x: grid_size.0,
+			bed_size,
+		}
+	}
+
+	fn empty() -> Self
+	{
+		Self {
+			points_correction: Vec::new(),
+			number_of_points_in_x: 0,
+			bed_size: Vector2::ZERO,
+		}
+	}
+
+	/// It uses bilinear interpolation.
+	fn get_point_correction(&self, point: Vector2) -> Distance
+	{
+		fn axis(point: Distance, bed_size_in_axis: Distance, number_of_points_in_axis: u8) -> f32
+		{
+			let point_in_bed = point / bed_size_in_axis;
+			point_in_bed * number_of_points_in_axis as f32
+		}
+		let x = axis(point.x(), self.bed_size.x(), self.number_of_points_in_x);
+		let y = axis(point.y(), self.bed_size.y(), self.number_of_points_in_y());
+
+		let interpolate_edge = |corner_xy: (u8, u8)| {
+			let edge_points_offsets = (
+				self.points_correction[self.xy_to_index(corner_xy.0, corner_xy.1)],
+				self.points_correction[self.xy_to_index(corner_xy.0, corner_xy.1 + 1)],
+			);
+
+			math::lerp(y.fract(), edge_points_offsets.0..=edge_points_offsets.1)
+		};
+
+		let left_edge_correction = (interpolate_edge)((x.trunc() as u8, y.trunc() as u8));
+		let right_edge_correction = (interpolate_edge)((1 + x.trunc() as u8, y.trunc() as u8));
+		math::lerp(x.fract(), left_edge_correction..=right_edge_correction)
+	}
+
+	pub fn index_to_xy(&self, index: u8) -> (u8, u8)
+	{
+		let x = index / self.number_of_points_in_x;
+		(x, index - (x * self.number_of_points_in_x))
+	}
+
+	fn xy_to_index(&self, x: u8, y: u8) -> usize
+	{
+		self.number_of_points_in_x as usize * y as usize + x as usize
+	}
+
+	fn number_of_points_in_y(&self) -> u8
+	{
+		(self.points_correction.len() / self.number_of_points_in_x as usize) as u8
+	}
+}
