532fe71d642f22b3443134eb555adbf74d5700fb
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-10-26 17:52:59 +0200

    📝 Add documentation to the cartridge heater driver module

--- firmware/crates/core/src/printer/components/drivers/cartridge_heater.rs
+++ firmware/crates/core/src/printer/components/drivers/cartridge_heater.rs
@@ -1,3 +1,9 @@
+//! This module provides the implementation for a cartridge heater interface
+//! that can be used to control the heating element in a 3D printer.
+//!
+//! The `CartridgeHeater` struct allows for PWM control of the heater's temperature
+//! through a specified pin.
+
 use crate::{printer::components::hal::pwm::PwmPin, utils::math::Percentage};
 
 /// A cartridge heater connected to the microcontroller that can be controlled using the `P` pin.
@@ -8,7 +14,10 @@ pub struct CartridgeHeater<P: PwmPin>
 
 impl<P: PwmPin> CartridgeHeater<P>
 {
-	/// Returns a [`CartridgeHeater`] that can control its heat percentage through the provided `pin`.
+	/// Returns a new [`CartridgeHeater`] that can control its heat percentage through the provided `pin`.
+	///
+	/// # Parameters
+	/// - `pin`: The PWM pin connected to the cartridge heater.
 	pub fn new(pin: P) -> Self
 	{
 		Self { pin }
