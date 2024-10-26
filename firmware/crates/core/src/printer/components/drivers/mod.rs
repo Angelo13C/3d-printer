23f3b57f6c7e4048bd489ca8badc874ae8fd7b54
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-10-26 17:53:18 +0200

    📝 Add documentation to the drivers module

--- firmware/crates/core/src/printer/components/drivers/mod.rs
+++ firmware/crates/core/src/printer/components/drivers/mod.rs
@@ -1,3 +1,9 @@
+//! This module contains implementations for various drivers that enable communication
+//! and control of components connected to the printer.
+//!
+//! Each submodule handles a specific component, providing the necessary functionality
+//! to interface with that component, ensuring proper operation within the printer system.
+
 pub mod bl_touch;
 pub mod button;
 pub mod cartridge_heater;
