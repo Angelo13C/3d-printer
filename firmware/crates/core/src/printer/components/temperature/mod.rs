ff30919d20b0e9900381e5c7ee13454b81469389
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-10-26 17:59:43 +0200

    📝 Add documentation to the temperature module

--- firmware/crates/core/src/printer/components/temperature/mod.rs
+++ firmware/crates/core/src/printer/components/temperature/mod.rs
@@ -1,3 +1,9 @@
+//! A module for managing temperature control in the 3D printer.
+//!
+//! This module includes components for temperature control, including PID (Proportional-Integral-Derivative)
+//! controllers and safety mechanisms to ensure safe operating temperatures. It provides the ability to adjust
+//! temperature settings, monitor temperature readings, and handle temperature-related errors.
+
 mod pid;
 pub mod safety;
 
