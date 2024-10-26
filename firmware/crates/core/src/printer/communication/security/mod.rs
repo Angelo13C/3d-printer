4aeedd2ec0dec48c90cec0400e4a777e432ee2d9
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-10-26 17:51:56 +0200

    📝 Add documentation to the security module

--- firmware/crates/core/src/printer/communication/security/mod.rs
+++ firmware/crates/core/src/printer/communication/security/mod.rs
@@ -1,3 +1,7 @@
+//! This module provides security functionality for communications with external systems.
+//! It ensures that HTTP requests are validated through password protection, brute-force
+//! prevention mechanisms...
+
 use std::time::Duration;
 
 use argon2::Params;
@@ -13,22 +17,26 @@ mod password;
 
 pub use ip_address::{GetIpAddress, IpAddress};
 
-/// A type that implements this trait is a layer of security used for the communication with the external world.
+/// A trait that defines a layer of security for communication with external systems.
 pub trait Protection
 {
 	type Input<'a>: ProtectionInput<'a>;
 
+	/// Checks if the input passes the protection criteria.
 	fn can_pass<'a>(&mut self, input: Self::Input<'a>) -> bool;
 }
-/// Input data required by [`Protection`] to check if the request is valid.
+
+/// Input data required by [`Protection`] to verify the validity of a request.
 pub trait ProtectionInput<'a>
 {
+	/// Generates input data from an HTTP request and resources.
 	fn generate<C: Connection, P: Peripherals>(
 		request: &'a mut Request<C>, resources: &mut MutexGuard<'_, ResourcesImpl<P>>,
 	) -> Result<Self, ()>
 	where Self: Sized;
 }
 
+/// Macro for testing protection mechanisms against a request.
 macro_rules! test_protection {
 	($protection_type: ty => $protection: expr, $request: expr, $resources: expr) => {
 		if let Some(protection) = $protection.as_mut()
@@ -48,6 +56,7 @@ macro_rules! test_protection {
 	};
 }
 
+/// A struct that encapsulates security mechanisms, including password and brute-force protection.
 pub struct Security
 {
 	password_protection: Option<PasswordProtection>,
@@ -56,6 +65,7 @@ pub struct Security
 
 impl Security
 {
+	/// Creates a new `Security` instance with the specified configuration.
 	pub fn new(configuration: Configuration) -> Result<Self, CreationError>
 	{
 		log::info!(
@@ -92,6 +102,7 @@ impl Security
 		})
 	}
 
+	/// Validates the request against the security criteria.
 	pub fn can_pass<C: Connection, P: Peripherals>(
 		&mut self, request: &mut Request<C>, resources: &mut MutexGuard<'_, ResourcesImpl<P>>,
 	) -> bool
@@ -103,6 +114,7 @@ impl Security
 	}
 }
 
+/// Possible errors that can occur during the creation of security mechanisms.
 #[derive(Debug)]
 pub enum CreationError
 {
@@ -110,12 +122,14 @@ pub enum CreationError
 	BruteForceProtection(argon2::password_hash::errors::Error),
 }
 
+/// Configuration settings for the `Security` struct.
 #[derive(Debug)]
 pub struct Configuration
 {
 	pub password: PasswordConfiguration,
 }
 
+/// Different configurations for password protection.
 #[derive(Debug)]
 pub enum PasswordConfiguration
 {
