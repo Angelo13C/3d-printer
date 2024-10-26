5918a0ab2ef8482126206503696dc5dcfff77d4c
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-10-26 18:03:46 +0200

    📝 Add documentation to the measurement module

--- firmware/crates/core/src/utils/measurement/mod.rs
+++ firmware/crates/core/src/utils/measurement/mod.rs
@@ -1,3 +1,6 @@
+//! This module provides types and functionality for various measurements,
+//! including angles, distances, durations, frequencies, and temperatures.
+
 pub mod angle;
 pub mod distance;
 pub mod duration;
