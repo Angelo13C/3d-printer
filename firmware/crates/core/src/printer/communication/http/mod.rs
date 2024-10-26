ff8f9b53515ef3a83e97c1dfd86bbba8a031cbe5
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-10-26 17:48:31 +0200

    📝 Add documentation to the http module

--- firmware/crates/core/src/printer/communication/http/mod.rs
+++ firmware/crates/core/src/printer/communication/http/mod.rs
@@ -1,3 +1,6 @@
+//! This module provides functionalities for handling HTTP communication within the firmware,
+//! including requests, resource management and ways to communicate to the components thread.
+
 pub mod command;
 pub mod other;
 pub mod request;
