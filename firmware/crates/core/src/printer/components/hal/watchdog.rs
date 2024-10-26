eb672807d4d09b94d955ebd10406675699e3fe96
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-10-26 17:56:17 +0200

    📝 Add documentation to the HAL watchdog module

--- firmware/crates/core/src/printer/components/hal/watchdog.rs
+++ firmware/crates/core/src/printer/components/hal/watchdog.rs
@@ -1,26 +1,38 @@
+//! Watchdog module.
+//!
+//! This module provides traits for creating and managing a [`Watchdog`] on the current thread.
+//!
+//! [`Watchdog`]: <https://en.wikipedia.org/wiki/Watchdog_timer>
+
 use std::fmt::Debug;
 
-/// A type that can instantiate a [`Watchdog`] on the current thread calling the [`Self::watch_current_thread`] method.
+/// A type that can instantiate a [`Watchdog`] on the current thread by calling the [`Self::watch_current_thread`] method.
 ///
-/// After you watch a thread you must regularly feed the returned watchdog, otherwise the firmware will go in an error state.
+/// After you watch a thread, you must regularly feed the returned watchdog; otherwise, the firmware will enter an error state.
 pub trait WatchdogCreator
 {
+	/// The type of watchdog that this creator produces.
 	type Watchdog: Watchdog;
 
-	/// Check the [`trait's`] documentation.
+	/// Creates a new watchdog for the current thread.
+	///
+	/// Check the [`trait's`] documentation for more information.
 	///
 	/// [`trait's`]: Self
 	fn watch_current_thread(self) -> Option<Self::Watchdog>;
 }
 
-/// A type that can restarts the [`watchdog timer`] in the microcontroller calling [`Self::feed`].
+/// A type that can restart the [`watchdog timer`] in the microcontroller by calling [`Self::feed`].
 ///
 /// [`watchdog timer`]: https://en.wikipedia.org/wiki/Watchdog_timer
 pub trait Watchdog
 {
+	/// The type of error that can occur while feeding the watchdog.
 	type Error: Debug;
 
-	/// Check the [`trait's`] documentation.
+	/// Feeds the watchdog to reset the timer and prevent the firmware from entering an error state.
+	///
+	/// Check the [`trait's`] documentation for more information.
 	///
 	/// [`trait's`]: Self
 	fn feed(&mut self) -> Result<(), Self::Error>;
