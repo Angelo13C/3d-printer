7f5cecf82a8ff1943d4be452d51f05603c6764f3
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-10-26 17:56:08 +0200

    📝 Add documentation to the HAL UART module

--- firmware/crates/core/src/printer/components/hal/uart.rs
+++ firmware/crates/core/src/printer/components/hal/uart.rs
@@ -1,15 +1,22 @@
+//! UART module.
+//!
+//! This module provides a trait for communication using the [`UART protocol`].
+//!
+//! [`UART protocol`]: <https://en.wikipedia.org/wiki/UART>
+
 use std::fmt::Debug;
 
 use crate::utils::measurement::duration::SmallDuration;
 
 /// A type that can be used to communicate using the [`UART protocol`].
 ///
-/// [`UART protocol`]: <https://it.wikipedia.org/wiki/UART>
+/// [`UART protocol`]: <https://en.wikipedia.org/wiki/UART>
 pub trait Uart
 {
+	/// The type of error that can occur while using the UART interface.
 	type Error: Debug;
 
-	/// Try to read `buf.len()` bytes from the interface and write them in `buf`.
+	/// Tries to read `buf.len()` bytes from the interface and writes them into `buf`.
 	/// If there are not enough readable bytes and the `timeout` expires, the function will return
 	/// without having read all the `buf.len()` bytes.
 	///
@@ -17,14 +24,14 @@ pub trait Uart
 	/// otherwise returns `Err(Self::Error)`.
 	fn read(&mut self, buf: &mut [u8], timeout: SmallDuration) -> Result<usize, Self::Error>;
 
-	/// Try to write `buf` in the interface.
+	/// Tries to write `buf` to the interface.
 	///
-	/// Returns `Ok(written_bytes_count)` if `written_bytes_count` were successfully written in the interface,
-	/// otherwise returns `Err(Self::Error)`;
+	/// Returns `Ok(written_bytes_count)` if `written_bytes_count` were successfully written to the interface,
+	/// otherwise returns `Err(Self::Error)`.
 	fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error>;
 
 	/// Discards all the bytes in the read buffer.
 	///
-	/// Returns `Ok(())` if the operation was succesful, otherwise returns `Err(Self::Error)`.
+	/// Returns `Ok(())` if the operation was successful, otherwise returns `Err(Self::Error)`.
 	fn flush_read(&mut self) -> Result<(), Self::Error>;
 }
